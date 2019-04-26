0e2c8ce40e1a838f19fa9caa01ea7e40c4730e27
Aerz <aerz94@gmail.com>
2019-04-26 21:35:03 +0200

    Update install_shell script

--- shell/install_shell.sh
+++ shell/install_shell.sh
@@ -15,8 +15,8 @@ brew install \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Spaceship ZSH (https://denysdovhan.com/spaceship-prompt)
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"
+ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 # Get z command
 git clone https://github.com/rupa/z.git /usr/local/etc/profile.d/z
