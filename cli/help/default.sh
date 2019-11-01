88b631bbed1ea60819e2e2ac78aeee1db1781f29
Agustín Cisneros <agustincc2@gmail.com>
2019-11-01 22:31:12 +0100

    Simplify the command execution

--- cli/help/default.sh
+++ cli/help/default.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-command_help
+echo
+echo "Usage:  dot <command> <sub_command>"
+echo
+echo "Commands:"
+echo "  configure"
+echo "    iterm                 Install all the iTerm configurations"
+echo "    vscode                Install VSCode configurations"
+echo "    zsh                   Install zsh customizations"
+echo ""
+echo "  macos"
+echo "    defaults              Set the macOS defaults"
+echo ""
+echo "  install"
+echo "    brew_apps             Install macOS apps using brew"
+echo "    app_store_apps        Install App Store apps using mas"
+echo ""
+echo "  help                    Show information about the cli usage"
+echo
