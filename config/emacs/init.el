a9f9848fe98257dc5b02b1ce81100a9c802e0b62
Agustín Cisneros <agustincc2@gmail.com>
2022-01-17 20:12:29 +0100

    Emacs: Add new packages

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -2,6 +2,7 @@
 ;; Package manager
 ;; ---
 (require 'package)
+
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     ;;'("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -13,9 +14,80 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; ---
+;; Packages
+;; ---
 (use-package which-key
   :ensure t
   :init (which-key-mode))
+
+(use-package avy
+  :ensure t
+  :bind ("C-:" . avy-goto-char))
+
+(use-package rainbow-mode
+  :ensure t
+  :init (rainbow-mode 1))
+
+(use-package helpful
+  :ensure t
+  :bind
+  ("C-h f" . helpful-callable)
+  ("C-h v" . helpful-variable)
+  ("C-h k" . helpful-key))
+
+;; ---
+;; Minibufer
+;; ---
+;; Autocompletion
+;; ido-mode + ido-vertical-mode + smex
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind ("M-x" . smex))
+
+;; ibuffer
+(global-set-key (kbd "C-x b") 'ibuffer)
+(setq ibuffer-expert t)
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+
+;; ---
+;; Windows
+;; ---
+(use-package switch-window
+  :ensure t
+  :config
+  (setq switch-window-input-style 'minibuffer)
+  (setq switch-window-increase 4)
+  (setq switch-window-threshold 2)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts
+	'("a" "s" "d" "f" "j" "k" "l" "i" "o"))
+  :bind ([remap other-window] . switch-window))
+
+(defun split-and-follow-horizontally ()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+
+(defun split-and-follow-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
 ;; ---
 ;; UI
@@ -40,6 +112,18 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; ---
+;; Code
+;; ---
+(global-subword-mode 1)
+
+(setq electric-pair-pairs
+      '(
+	(?\( . ?\))
+	(?\[ . ?\))
+	))
+(electric-pair-mode 1)
+
+;; ---
 ;; org-mode
 ;; ---
 (require 'org-tempo)
@@ -48,16 +132,16 @@
   :ensure t
   :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
 
-;; ---
-;; Self-added variables
-;; ---
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wombat))
- '(package-selected-packages '(org-bullets which-key use-package)))
+ '(package-selected-packages
+   '(helpful switch-window rainbow-mode avy smex ido-vertical-mode org-bullets which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
