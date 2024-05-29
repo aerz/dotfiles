e866d3a46d8ddd82f8041d0cc205a25b57e4bca1
Agustín Cisneros <agustincc@tutanota.com>
2024-05-29 09:41:13 +0200

    update polybar

--- config/polybar/polybar.sh
+++ config/polybar/polybar.sh
@@ -6,15 +6,15 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 5; done
 case "$(hostname)" in
     gigavac)
         { while :; do
-              polybar --reload --config="$HOME/.config/polybar/config.gigavac.ini" monitor1
+              polybar --reload --config="$HOME/.config/polybar/gigavac.ini" main
           done } & disown
 
         { while :; do
-              polybar --reload --config="$HOME/.config/polybar/config.gigavac.ini" monitor2
+              polybar --reload --config="$HOME/.config/polybar/gigavac.ini" right
           done } & disown
     ;;
     minivac)
-        polybar --reload --config="$HOME/.config/polybar/minivac.ini" laptop 2>&1 | \
+        polybar --reload --config="$HOME/.config/polybar/minivac.ini" main 2>&1 | \
             tee -a /tmp/polybar.laptop.log & disown
     ;;
 esac
