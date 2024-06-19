2629ee5763f00401066df4e6926c82fe0799abaa
Agustín Cisneros <agustincc@tutanota.com>
2024-06-19 12:33:32 +0200

    fix aur rpc limit reach 429 error
    * updates.sh script was restarting on every lock/unlock session causing
    infinite loops even using interval setting

--- .config/waybar/scripts/updates.sh
+++ .config/waybar/scripts/updates.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
+#
+# Package updates indicator module script.
+#
+# ------------------------------------------------------------------------------
+# DEPENDENCIES
+# ------------------------------------------------------------------------------
+# paru (https://github.com/Morganamilo/paru/)
+# pacman-contrib
 
-PKG_UPDATE_MIN=50
-
-is_pacman_running() {
-    pgrep "paru|yay" >/dev/null 2>&1
+is_pkgmgr_running() {
+    pgrep "paru|pacman" &>/dev/null
     test $? -eq 0
 }
 
-show_updates() {
+if is_pkgmgr_running; then
+    echo ""
+    sleep 600 # 10m
+fi
+
+while true; do
     pacman=$(checkupdates 2>/dev/null | wc -l)
-    aur=$(yay -Qum 2>/dev/null | wc -l)
+    aur=$(/usr/bin/paru -Qum 2>/dev/null | wc -l)
     updates=$(( pacman + aur ))
 
-    [ $updates -ge $PKG_UPDATE_MIN ] && echo "󰏗 $updates" || echo ""
-}
-
-is_pacman_running && echo ""
-show_updates
+    [ $updates -ge 0 ] && echo "󰏗 $updates" || echo ""
+    sleep 21600 # 6h
+done
