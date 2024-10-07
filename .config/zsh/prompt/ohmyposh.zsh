c046f52d747caa80b8d9c69dfccfc91ad335bfdc
Agustín Cisneros <agustincc@tutanota.com>
2024-10-07 10:15:04 +0200

    rm prompt headers

--- .config/zsh/prompt/ohmyposh.zsh
+++ .config/zsh/prompt/ohmyposh.zsh
@@ -1,6 +1,4 @@
-# ------------------------------------------------------------------------------
-# oh my posh
-# ------------------------------------------------------------------------------
+#
 # https://github.com/jandedobbeleer/oh-my-posh
 #
 eval "$(oh-my-posh init --config "${XDG_CONFIG_HOME:-${HOME}/.config}/ohmyposh.toml" zsh)"
